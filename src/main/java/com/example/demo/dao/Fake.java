8640b0469df891aabccfe14d8bfe2adfb0cff7e3
Anurag Dutta <imanurag18@gmail.com>
2023-05-25 19:36:11 +0530

    Spring boot file is pushed

---
+++ src/main/java/com/example/demo/dao/Fake.java
@@ -0,0 +1,17 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Fake implements PersonDao {
+    public static List<Person> DB = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+}
