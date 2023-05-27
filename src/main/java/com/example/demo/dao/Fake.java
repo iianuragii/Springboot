519ed974295bbf1c4620a4dcb66b8255e01d39f8
Anurag Dutta <imanurag18@gmail.com>
2023-05-27 13:19:36 +0530

    changes made

--- src/main/java/com/example/demo/dao/Fake.java
+++ src/main/java/com/example/demo/dao/Fake.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Repository;
+
+@Repository("fakeDao")
 public class Fake implements PersonDao {
     public static List<Person> DB = new ArrayList<>();
 
@@ -13,5 +16,10 @@ public class Fake implements PersonDao {
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople(){
+        return DB
     }
 }
--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -2,12 +2,14 @@ package com.example.demo.model;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Person {
 
     private final UUID id;
     private final String name;
 
-    public Person(UUID id, String name) {
+    public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
